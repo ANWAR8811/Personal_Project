5fa2920093d30834e60669a870b43e6f2fa9b127
MD. ANWAR HOSSAIN <104732602+ANWAR8811@users.noreply.github.com>
2023-06-09 13:51:13 +0600

    Add files via upload

---
+++ Java/mavenproject2/src/main/java/com/mycompany/mavenproject2/BioData.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject2;
+
+public class BioData {
+    public static void main(String[] args) {
+        System.out.println("Rana Ahamed");
+        System.out.print("Anwar Hossain");
+        
+    }  
+}
